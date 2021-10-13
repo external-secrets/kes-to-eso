47bf9606ddaea10c7192ae9c9aedfa75aecff4da
Gustavo Carvalho <gustavo.carvalho@container-solutions.com>
2021-10-13 16:19:27 -0300

    Started to add test structure
    Signed-off-by: Gustavo Carvalho <gustavo.carvalho@container-solutions.com>

---
+++ pkg/utils/utils_test.go
@@ -0,0 +1,25 @@
+package utils
+
+import (
+	"testing"
+)
+
+func TestInitKubeContig(t *testing.T) {
+
+}
+
+func TestIsKES(t *testing.T) {
+
+}
+
+func TestGetSecretValue(t *testing.T) {
+
+}
+
+func TestGetServiceAccountIfAnnotationExists(t *testing.T) {
+
+}
+
+func TestUpdateOrCreateSecret(t *testing.T) {
+
+}
